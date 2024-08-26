f4ddd0fd3311e726dc4e35d1bc75b896d430da02
LEIGENGEN <905627019@qq.com>
2024-08-26 19:34:26 +0800

    项目搭建-header组件

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,69 @@
+<script setup>
+import {ref, computed} from 'vue'
+// 当前绝对的路径asset找到这个资源 new URL (url,base)
+const getImageUrl = (user) => new URL(`../assets/images/${user}.png`, import.meta.url)
+</script>
+
+<template>
+  <div class="header">
+    <div class="l-content">
+      <el-button size="small">
+        <component class="icons" is="menu"></component>
+      </el-button>
+      <el-breadcrumb separator="/" class="bread">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="r-content">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+            <img :src="getImageUrl('user')" class="user">
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+}
+
+.icons {
+  width: 20px;
+  height: 20px;
+}
+
+.l-content {
+  display: flex;
+  align-items: center;
+
+  .el-button {
+    margin-right: 20px;
+  }
+}
+
+.r-content {
+  .user {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+
+:deep(.bread span) {
+  color: #fff !important;
+  cursor: pointer !important;
+}
+</style>
