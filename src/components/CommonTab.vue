cdfb96af6311dd83fb94d7acb955a2f2896870f0
LEIGENGEN <905627019@qq.com>
2024-08-28 14:15:30 +0800

    tag静态实现

---
+++ src/components/CommonTab.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from 'vue'
+import {useRoute} from 'vue-router'
+
+const route = useRoute()
+const tags = ref([
+  {
+    path: '/home',
+    name: 'home',
+    label: '首页',
+    icon: 'home'
+  }
+])
+</script>
+
+<template>
+  <div class="tabs">
+    <el-tag v-for="(item,index) in tags" :key="index" :closable="item.name!=='home'" :effect="route.name===item.name?'dark':'plain'">{{ item.label }}</el-tag>
+  </div>
+</template>
+
+<style scoped lang="less">
+.tabs {
+  margin: 20px 0 0 20px;
+}
+.el-tag{
+  margin-right: 10px;
+}
+</style>
