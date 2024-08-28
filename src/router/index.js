23b6edf4bb8e6e960bd93eed03dfbee0a202e614
LEIGENGEN <905627019@qq.com>
2024-08-28 19:28:20 +0800

    登陆页面静态搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,31 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
-const routes = [{
-  path: '/',
-  name: 'main',
-  component: () => import('../views/Main.vue'),
-  redirect: '/home',
-  children: [{
-    path: '/home',
-    name: 'home',
-    component: () => import('../views/Home.vue')
-  }, {
-    path: '/user',
-    name: 'user',
-    component: () => import('../views/User.vue')
-  }, {
-    path: '/mall',
-    name: 'mall',
-    component: () => import('../views/Mall.vue')
-  }]
-}]
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../views/Main.vue'),
+    redirect: '/home',
+    children: [{
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    }, {
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/User.vue')
+    }, {
+      path: '/mall',
+      name: 'mall',
+      component: () => import('../views/Mall.vue')
+    }]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
